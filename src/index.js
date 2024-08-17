1afad6015df1b1dae5704a15d82991e1393970ff
valen2004vega <valen2004vega@gmail.com>
2024-08-16 19:32:55 -0500

    feat: add appearing validating message

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import validator from './validator.js';
 
-console.log(validator);
+const validateCardNumber = () => {
+  const cardNumber = document.getElementById('card-number').value;
+  const isValidCard = validator.isValid(cardNumber);
+  const validationMsg = isValidCard ? 'TU TARJETA FUE VALIDADA CON EXITO': 'TU TARJETA NO ES VALIDA';
+  document.getElementById('validation-msg').textContent = validationMsg;
+  document.getElementById('validation-msg').style.display = 'block';
+}
+
+const verifyBtn = document.getElementById('verify-btn');
+verifyBtn.addEventListener('click', () => {
+  validateCardNumber();
+})
