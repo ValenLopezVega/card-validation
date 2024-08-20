05627f28d4e268979334ee7d33c457f27947bd5c
valen2004vega <valen2004vega@gmail.com>
2024-08-20 11:26:14 -0500

    feat: applied maskify

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import validator from './validator.js';
 const validateCardNumber = () => {
   const cardNumber = document.getElementById('card-number').value;
   const isValidCard = validator.isValid(cardNumber);
-  const validationMsg = isValidCard ? 'TU TARJETA FUE VALIDADA CON EXITO': 'TU TARJETA NO ES VALIDA';
+  const input = document.getElementById('card-number');
+  const maskedValue = validator.maskify(input.value);
+  const validationMsg = isValidCard ? `${maskedValue} ES UNA TARJETA VALIDA`: `${maskedValue} NO ES UNA TARJETA VALIDA`;
+
   document.getElementById('validation-msg').textContent = validationMsg;
   document.getElementById('validation-msg').style.display = 'block';
+  document.getElementById('card-number').style.display = 'none';
 }
 
 const verifyBtn = document.getElementById('verify-btn');
